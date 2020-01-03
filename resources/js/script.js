069b8e405fa6120f324fef753dfba54b5430f616
Alexis Rengifo <alexisrengifo@gmail.com>
2020-01-03 12:38:08 -0500

    Tab switching behavior

--- resources/js/script.js
+++ resources/js/script.js
@@ -7,7 +7,7 @@ function ready(fn) {
 }
 
 ready(() => {
-    // Resizing input inline text controls
+    // Resizing input inline text controls.
     let elements = document.querySelectorAll('.form-row--inline input.form__input[type="text"]');
     Array.prototype.forEach.call(elements, function (el, i) {
         el.parentNode.querySelectorAll('.form__text')[0].innerHTML = el.value;
@@ -20,5 +20,47 @@ ready(() => {
             e.preventDefault();
             el.parentNode.querySelectorAll('form')[0].classList.toggle('form--noedit');
         })
-    })
+    });
+
+    let activateTab = (e) => {
+
+        if (e.classList.contains('horizontal-menu__link--active'))
+            return;
+
+        let hm = e.parentNode.parentNode;
+
+        // Deactivate active items.
+        let activeitems = hm.querySelectorAll('.horizontal-menu__link--active');
+        Array.prototype.forEach.call(activeitems, (el, i) => {
+            el.parentNode.classList.remove('horizontal-menu__item--active');
+            el.classList.remove('horizontal-menu__link--active');
+        });
+
+        // Activate clicked item and its parent.
+        e.classList.add('horizontal-menu__link--active');
+        e.parentNode.classList.add('horizontal-menu__item--active');
+
+        // Make infocards visible.
+        let eid = e.parentNode.id;
+        let infocards = hm.nextElementSibling.querySelectorAll('.infocard');
+        Array.prototype.forEach.call(infocards, (el, i) => {
+            if (!el.classList.contains(eid)) {
+                el.style.display = 'none';
+            } else {
+                el.style.display = '';
+            }
+        });
+    }
+
+    // Horizontal menu behavior.
+    let hormenus = document.querySelectorAll('.horizontal-menu');
+    Array.prototype.forEach.call(hormenus, (el, i) => {
+        let links = el.querySelectorAll('.horizontal-menu__link');
+        Array.prototype.forEach.call(links, (el, i) => {
+            el.addEventListener('click', (e) => {
+                e.preventDefault();
+                activateTab(e.target);
+            });
+        })
+    });
 })
